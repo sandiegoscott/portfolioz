79945d5d5d4d58da7defa6267af810b94c04da6b
Scott Olmsted <solmsted@gmail.com>
2015-03-03 12:54:07 -0500

    Make changes to some models

--- app/models/transaction/short.rb
+++ app/models/transaction/short.rb
@@ -18,12 +18,15 @@ class Transaction::Short < ActiveType::Record[Transaction]
   private
 
   def compute_cash_delta
-    #puts ">>>>> /Transaction::Short#compute_cash_delta/"
     if shares.nil? || price.nil?
       self.cash_delta = nil
     else
       self.cash_delta = shares * price - commission
     end
+  end
+
+  def compute_shares_delta
+    self.shares_delta = -shares
   end
 
 end
