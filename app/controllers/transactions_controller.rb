315a0a33efb62d6c85a21114df078a51348bc463
Scott Olmsted <solmsted@gmail.com>
2015-03-05 15:10:58 -0500

    Rename xransfer_controller to transactions_controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,47 @@
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @transactions = Transaction.all
+    respond_with(@transactions)
+  end
+
+  def show
+    respond_with(@transaction)
+  end
+
+  def new
+    @transaction = Transaction.new
+    respond_with(@transaction)
+  end
+
+  def edit
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.save
+    respond_with(@transaction)
+  end
+
+  def update
+    @transaction.update(transaction_params)
+    respond_with(@transaction)
+  end
+
+  def destroy
+    @transaction.destroy
+    respond_with(@transaction)
+  end
+
+  private
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    def transaction_params
+      params[:transaction]
+    end
+end
